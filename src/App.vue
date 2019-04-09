61cd0686b4caaf6e1eb3c2b90539f87186c96293
leixiaopeng <2672947175@qq.com>
2019-04-09 09:42:41 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
+*{
+  margin: 0;
+  padding: 0;
+}
+ul li{
+  list-style: none;
+}
+a{
+  text-decoration: none;
+}
+
 #app {
+ 
+  margin: 0 auto;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -17,7 +29,6 @@
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
@@ -26,4 +37,27 @@
     }
   }
 }
+.heder{
+        width: 100%;
+        height: 50px;
+        background: #ccc;
+        line-height: 50px;
+        color: #fff;
+        font-size: 20px;
+        .a1{
+            float: left;
+            padding: 0 50px;
+            i{
+                font-size: 28px;
+                color: #fff;
+            }
+        }
+        .a2{
+            float: right;
+            padding: 0 50px;
+            color: #fff;
+            font-size: 20px;
+            cursor: pointer;
+        }
+    }
 </style>
